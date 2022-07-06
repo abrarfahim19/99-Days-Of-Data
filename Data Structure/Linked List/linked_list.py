cb935897aa15e41d7b4a59eda396b22aa3916d9f
abrarfahim19 <fahimpatwari19@gmail.com>
2022-07-06 23:57:02 +0600

    Linked List Two Method

--- Data Structure/Linked List/linked_list.py
+++ Data Structure/Linked List/linked_list.py
@@ -8,10 +8,6 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def insert_at_begining(self, data):
-        node = Node(data, self.head)
-        self.head = node
-
     def print(self):
         if self.head is None:
             print("The list is Empty")
@@ -24,9 +20,24 @@ class LinkedList:
             itr = itr.next
         print(llstr)
 
+    def insert_at_begining(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def insert_at_end(self, data):
+        if self.head is None:
+            self.head = Node(data, None)
+
+        itr = self.head
+        while itr.next:
+            itr = itr.next
+
+        itr.next = Node(data, None)
+
 
 if __name__ == "__main__":
     ll = LinkedList()
     ll.insert_at_begining(10)
     ll.insert_at_begining(12)
+    ll.insert_at_end(2)
     ll.print()
